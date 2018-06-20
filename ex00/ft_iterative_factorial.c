ba88e5103300577dd2304061045bc7d71ec7e8c6
Thomas Charlat <thomas.charlat@gmail.com>
2018-06-21 00:03:01 +0200

    Update ft_iterative_factorial.c

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -1,27 +1,11 @@
 int	ft_iterative_factorial (int nb)
 {
-	int counter;
 	int result;
-	double copyresult;
 
-	counter = 1;
 	result = 1;
-	copyresult = 1;
-
-	if (nb>0)
+	while (nb > 0)
 	{
-	while ((counter <= nb) && (result == copyresult))
-		{
-			result = result * counter;
-			copyresult = copyresult * counter;
-			counter++;
-		}
-		if (result == copyresult)
-			return (result);
-		else return (0);
+		result *= nb--;
 	}
-	else return (0);
+	return (result);
 }
-
-
-
