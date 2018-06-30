03e27a558598934e7df5e4781aed76b30b026f52
Thomas Charlat <thomas.charlat@gmail.com>
2018-06-30 08:23:21 +0200

    Update ft_iterative_factorial.test.c

--- __tests__/ex00/ft_iterative_factorial.test.c
+++ __tests__/ex00/ft_iterative_factorial.test.c
@@ -13,21 +13,27 @@ void    assertIntegerEqual(int expected, int received)
 
 int     main()
 {
-    printf("When argument is -1 then return should be 0");
+    printf("When argument is -1 then return should be 0\n");
     assertIntegerEqual(0, ft_iterative_factorial(-1));
+    printf("OK\n");
 
-    printf("When argument is 0 then return should be 0");
+    printf("When argument is 0 then return should be 0\n");
     assertIntegerEqual(0, ft_iterative_factorial(0));
+    printf("OK\n");
 
-    printf("When argument is 1 then return should be 1");
+    printf("When argument is 1 then return should be 1\n");
     assertIntegerEqual(1, ft_iterative_factorial(1));
+    printf("OK\n");
 
-    printf("When argument is 2 then return should be 2");
+    printf("When argument is 2 then return should be 2\n");
     assertIntegerEqual(2, ft_iterative_factorial(2));
+    printf("OK\n");
 
-    printf("When argument is 3 then return should be 6");
+    printf("When argument is 3 then return should be 6\n");
     assertIntegerEqual(6, ft_iterative_factorial(3));
+    printf("OK\n");
 
-    printf("When argument is 11 then return should be 39916800");
+    printf("When argument is 11 then return should be 39916800\n");
     assertIntegerEqual(39916800, ft_iterative_factorial(11));
+    printf("OK\n");
 }
